1e74d09581871f9898c838d6a935a59488775abc
motopeco <peco@pelife.jp>
2024-03-05 12:10:58 +0900

    add setting for npm

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,40 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import typescript from "@rollup/plugin-typescript";
+import { typescriptPaths } from "rollup-plugin-typescript-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      name: "LoadingProvider",
+      name: "react-loading-provider",
       fileName: "react-loading-provider",
-      formats: ["es", "umd", "cjs"],
+      formats: ["es", "cjs"],
+    },
+    manifest: true,
+    minify: true,
+    reportCompressedSize: true,
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "React-dom",
+        },
+        entryFileNames: "[name].js",
+      },
+      plugins: [
+        typescriptPaths({
+          preserveExtensions: true,
+        }),
+        typescript({
+          sourceMap: false,
+          declaration: true,
+          outDir: "dist",
+        }),
+      ],
     },
   },
   plugins: [react()],
